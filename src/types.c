df4f0cb6b1424dde2dbd15ab422b9beb64800fd4
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-11-18 22:29:32 +0000

    types limits

--- src/types.c
+++ src/types.c
@@ -22,4 +22,16 @@ GSHL_TEST(types_size)
     GSHL_TEST_EQUAL(sizeof(bool), 1);
 }
 
+GSHL_TEST(types_limits)
+{
+    GSHL_TEST_EQUAL((u8)(U8_MAX + 1), 0);
+    GSHL_TEST_EQUAL((u8)(U8_MIN - 1), U8_MAX);
+
+    GSHL_TEST_EQUAL((u16)(U16_MAX + 1), 0);
+    GSHL_TEST_EQUAL((u16)(U16_MIN - 1), U16_MAX);
+
+    GSHL_TEST_EQUAL((u32)(U32_MAX + 1), 0);
+    GSHL_TEST_EQUAL((u32)(U32_MIN - 1), U32_MAX);
+}
+
 #endif
