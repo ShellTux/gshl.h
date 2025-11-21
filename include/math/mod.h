a23af55d48c1b91553fd5ccbf2076d22046f0e80
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-11-21 03:17:26 +0000

    fix(math): strip prefix

--- include/math/mod.h
+++ include/math/mod.h
@@ -3,6 +3,14 @@
 
 #include "types/mod.h"
 
+/// {{{ Functions
+
 usize GSHL_factorial(const usize number);
+
+/// }}}
+
+#ifdef GSHL_STRIP_PREFIX
+#    define factorial GSHL_factorial
+#endif
 
 #endif // INCLUDE_INCLUDE_MATH_H_
