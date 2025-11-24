ae83eb46f871d948f9a8c90769a55b238054188b
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-11-24 07:01:21 +0000

    build: don't build benches every time

--- targets.mk
+++ targets.mk
@@ -4,12 +4,13 @@ TARGETS = \
 					$(TARGET_DIR)/examples/hash-table \
 					$(TARGET_DIR)/examples/macros \
 					$(TARGET_DIR)/examples/print \
-					$(TARGET_DIR)/benches/print \
-					$(TARGET_DIR)/benches/print.flamegraph.svg \
-					$(TARGET_DIR)/benches/hash-table \
-					$(TARGET_DIR)/benches/hash-table.flamegraph.svg \
 					$(TARGET_DIR)/libgshl.so \
 					$(TARGET_DIR)/libgshl.a
+
+.PHONY: benchmark
+benchmark: \
+	$(TARGET_DIR)/benches/print.flamegraph.svg \
+	$(TARGET_DIR)/benches/hash-table.flamegraph.svg
 
 DEPS := $(SRCS)
 $(TARGET_DIR)/libgshl.so: $(DEPS:%.c=$(BUILD_DIR)/%.o)
