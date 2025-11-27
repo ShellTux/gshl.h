00b847375fc216b9b432e40a67fdb29f54fa3c22
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-11-27 02:05:46 +0000

    refactor targets.mk

--- targets.mk
+++ targets.mk
@@ -29,45 +29,9 @@ $(TARGET_DIR)/gshl-test: $(DEPS:%.c=$(BUILD_DIR)/%.o)
 	@mkdir --parents $(shell dirname $@)
 	$(CC) -o $@ $^ $(LDFLAGS) -lm
 
-DEPS := examples/macros.c
-$(TARGET_DIR)/examples/macros: $(DEPS:%.c=$(BUILD_DIR)/%.o)
+$(TARGET_DIR)/%: $(BUILD_DIR)/%.o
 	@mkdir --parents $(shell dirname $@)
 	$(CC) -o $@ $^ $(LDFLAGS)
-
-DEPS := examples/print.c
-$(TARGET_DIR)/examples/print: $(DEPS:%.c=$(BUILD_DIR)/%.o)
-	@mkdir --parents $(shell dirname $@)
-	$(CC) -o $@ $^ $(LDFLAGS)
-
-DEPS := examples/dynamic-array.c
-$(TARGET_DIR)/examples/dynamic-array: $(DEPS:%.c=$(BUILD_DIR)/%.o)
-	@mkdir --parents $(shell dirname $@)
-	$(CC) -o $@ $^ $(LDFLAGS)
-
-DEPS := examples/hash.c
-$(TARGET_DIR)/examples/hash: $(DEPS:%.c=$(BUILD_DIR)/%.o)
-	@mkdir --parents $(shell dirname $@)
-	$(CC) -o $@ $^ $(LDFLAGS)
-
-DEPS := examples/hash-table.c
-$(TARGET_DIR)/examples/hash-table: $(DEPS:%.c=$(BUILD_DIR)/%.o)
-	@mkdir --parents $(shell dirname $@)
-	$(CC) -o $@ $^ $(LDFLAGS)
-
-DEPS := examples/string.c
-$(TARGET_DIR)/examples/string: $(DEPS:%.c=$(BUILD_DIR)/%.o)
-	@mkdir --parents $(shell dirname $@)
-	$(CC) -o $@ $^ $(LDFLAGS)
-
-DEPS := benches/print.c
-$(TARGET_DIR)/benches/print: $(DEPS:%.c=$(BUILD_DIR)/%.o)
-	@mkdir --parents $(shell dirname $@)
-	$(CC) -pg -o $@ $^ $(LDFLAGS) -lm
-
-DEPS := benches/hash-table.c
-$(TARGET_DIR)/benches/hash-table: $(DEPS:%.c=$(BUILD_DIR)/%.o)
-	@mkdir --parents $(shell dirname $@)
-	$(CC) -pg -o $@ $^ $(LDFLAGS) -lm
 
 %.flamegraph.svg: %
 	flamegraph --output $@ -- $< >/dev/null
