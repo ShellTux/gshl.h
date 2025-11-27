fa3d0e779ce33a12ba24eaf4c916b5b750c501f1
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-11-27 02:04:06 +0000

    feat math is_power_of_two

--- include/math/mod.h
+++ include/math/mod.h
@@ -6,11 +6,13 @@
 /// {{{ Functions
 
 usize GSHL_factorial(const usize number);
+bool GSHL_is_power_of_two(const usize number);
 
 /// }}}
 
 #ifdef GSHL_STRIP_PREFIX
 #    define factorial GSHL_factorial
+#    define is_power_of_two GSHL_is_power_of_two
 #endif
 
 #endif // INCLUDE_MATH_MOD_H_
--- src/math/mod.c
+++ src/math/mod.c
@@ -10,6 +10,11 @@ usize GSHL_factorial(const usize number)
     return result;
 }
 
+bool GSHL_is_power_of_two(const usize number)
+{
+    return (number > 0) && !(number & (number - 1));
+}
+
 #ifdef GSHL_TESTS
 
 #    include "test/mod.h"
@@ -24,6 +29,31 @@ GSHL_TEST(factorial)
     GSHL_TEST_EQUAL(GSHL_factorial(5), 120);
     GSHL_TEST_EQUAL(GSHL_factorial(6), 720);
     GSHL_TEST_EQUAL(GSHL_factorial(7), 5040);
+}
+
+GSHL_TEST(is_power_of_two)
+{
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(0), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(1), true);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(2), true);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(3), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(4), true);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(5), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(6), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(7), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(8), true);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(9), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(10), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(11), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(12), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(13), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(14), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(15), false);
+    GSHL_TEST_EQUAL(GSHL_is_power_of_two(16), true);
+
+    for (u8 i = 0; i < 64; ++i) {
+        GSHL_TEST_EQUAL(GSHL_is_power_of_two(1UL << i), true);
+    }
 }
 
 #endif
